8164a62e6eafd9ba932bea690750dfd102be61bd
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2020-06-18 00:49:49 +0200

    j

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.awt.*;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Map mapa = new Map(new MapTransformer(new MapGenerator(100,100)));
         Jugador jugador = new Jugador();
